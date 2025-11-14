1a62d947669240592f565f992f7b3de395431f4f
Maruf-Raduan <raduanulhaque@gmail.com>
2025-11-14 19:07:25 +0000

    Fix Replit configuration

--- app.py
+++ app.py
@@ -22,4 +22,6 @@ def chat():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
